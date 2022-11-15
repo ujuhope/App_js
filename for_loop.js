b12331ddda343d747de04c6ef7a708fb2400375c
Michael Hope Chukwu <78309364+ujuhope@users.noreply.github.com>
2022-11-15 05:57:35 +0100

    for loop

---
+++ for_loop.js
@@ -0,0 +1,31 @@
+// loop through the code below and print out a sentence of this format.
+// My name is ......., I am ........ old and I am in class ....
+var classStudentsDetails = [
+    {
+        name: "Princess",
+        age:2,
+        class:"js1"
+    },
+    {
+        name: "Hope",
+        age:1,
+        class:"js2"
+    },
+    {
+        name: "Amarachi",
+        age:3,
+        class:"js3"
+    },
+    {
+        name: "Vivian",
+        age:2,
+        class:"js1"
+    }
+
+];
+
+for(var i = 0;  i < classStudentsDetails.length; i++){
+console.log("My name is" + " " + classStudentsDetails[i] .name + "," +
+ " " + "I am"+ " " + classStudentsDetails [i].age + " " + "years old"+
+ " " + "and I am in" + " " + classStudentsDetails[i] .class);
+} 
